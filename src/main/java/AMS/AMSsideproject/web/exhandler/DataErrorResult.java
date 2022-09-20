c6098682277e9bd6135c3bdf8bac3321f6192e19
BonSik-Koo <rnqhstlr2297@gmail.com>
2022-09-20 23:14:08 +0900

    cloud test

--- src/main/java/AMS/AMSsideproject/web/exhandler/DataErrorResult.java
+++ src/main/java/AMS/AMSsideproject/web/exhandler/DataErrorResult.java
@@ -5,12 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DataErrorResult<T> extends BaseErrorResult{
 
-    //@ApiModelProperty(dataType = "[AMSsideproject.web.exhandler.advice.UserValidExceptionDto;")
     private T data;
 
     public DataErrorResult(String message, String code, String status, T data) {
