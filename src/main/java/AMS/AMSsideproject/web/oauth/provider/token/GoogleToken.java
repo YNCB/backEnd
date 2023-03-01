0b8652371638b3a205869453ad038a2423b5924b
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-03-01 20:19:55 +0900

    fix:구글 oauth redirect url 수정

--- src/main/java/AMS/AMSsideproject/web/oauth/provider/token/GoogleToken.java
+++ src/main/java/AMS/AMSsideproject/web/oauth/provider/token/GoogleToken.java
@@ -7,7 +7,7 @@ public class GoogleToken {
 
     private String access_token;
     private String token_type;
-    //private String refresh_token;
+    private String refresh_token;
     private Integer expires_in;
     private String scope;
     private String id_token;
